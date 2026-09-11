989cbcf11e62071f2b0727338312196bbc84071b
Hamza Benghelal <hamza.benghelal@laplateforme.io>
2026-09-11 10:29:09 +0200

    Mise a jour des liens de contact

--- script.js
+++ script.js
@@ -21,7 +21,7 @@ updateStickyHeader();
 const internalLinks = document.querySelectorAll('a[href^="#"]');
 
 internalLinks.forEach((link) => {
-  link.addEventListener("click", () => {
+  link.addEventListener("click", (event) => {
     const targetId = link.getAttribute("href");
 
     if (!targetId || targetId === "#") {
@@ -33,6 +33,8 @@ internalLinks.forEach((link) => {
     if (!target) {
       return;
     }
+
+    event.preventDefault();
 
     target.scrollIntoView({
       behavior: "smooth",
